820138d3631d3a148bd1f36f7e7ce1b2e6b3cd25
Rakshit Jha <jrakshit749@gmail.com>
2021-06-23 22:16:43 +0530

    The snake moves in the right direction without any input, it looks cool though...

--- game.js
+++ game.js
@@ -1,12 +1,27 @@
+import { SNAKE_SPEED, update as updateSnake, draw as drawSnake} from "./snake.js"
+
 let lastRenderTime = 0
-const SNAKE_SPEED = 2
+
+const gameBoard = document.getElementById('game-board')
 
 function main(currentTime){
+    window.requestAnimationFrame(main)
     const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
     if (secondsSinceLastRender < 1/ SNAKE_SPEED) return
-
-
     lastRenderTime = currentTime
-    window.requestAnimationFrame(main)
+    
+    update()
+    draw()
+}
+
+window.requestAnimationFrame(main)
+
+function update(){
+    updateSnake()
+}
+
+function draw(){
+    gameBoard.innerHTML = ''
+    drawSnake(gameBoard)
 
 }
