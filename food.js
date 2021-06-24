2a27d506d5910b152c34e8e48fb4201fc995e084
Rakshit Jha <jrakshit749@gmail.com>
2021-06-24 23:38:15 +0530

    The snake now expands after eating the first food

--- food.js
+++ food.js
@@ -1,13 +1,29 @@
+import { onSnake, expandSnake } from "./snake.js"
+
+  
+let food = getRandomFoodPosition()
+const EXPANSION_RATE = 1
+
+
 export function update(){
-    
+    if (onSnake(food)){
+        expandSnake(EXPANSION_RATE)
+        food=getRandomFoodPosition()
+    }
 }
 
 export function draw(gameBoard){
-    snakeBody.forEach(segment=>{
-        const snakeElement = document.createElement('div')
-        snakeElement.style.gridRowStart = segment.y
-        snakeElement.style.gridColumnStart = segment.x
-        snakeElement.classList.add('snake')
-        gameBoard.appendChild(snakeElement)
-    })
+    const foodElement = document.createElement('div')
+    foodElement.style.gridRowStart = food.y
+    foodElement.style.gridColumnStart = food.x
+    foodElement.classList.add('food')
+    gameBoard.appendChild(foodElement)
+
+}
+
+function getRandomFoodPosition(){
+    let newFoodPosition
+    while(newFoodPosition == null || onSnake(newFoodPosition)){
+        newFoodPosition=randomGridPosition()
+    }
 }
