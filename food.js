5c91a7e28c4de9fc67548545564ef0dc631ec6fd
Rakshit Jha <jrakshit749@gmail.com>
2021-06-25 00:53:37 +0530

    Completed the Game, Happy Snakey Trails!

--- food.js
+++ food.js
@@ -2,7 +2,7 @@ import { onSnake, expandSnake } from "./snake.js"
 import { randomGridPosition } from "./grid.js"
   
 let food = getRandomFoodPosition()
-const EXPANSION_RATE = 10
+const EXPANSION_RATE = 1
 
 
 export function update(){
